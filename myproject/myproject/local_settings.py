980552812c6619402f80fbbb0030fbf5f71f2c44
tabira2382 <tabira2382@gmail.com>
2021-01-19 11:49:55 +0900

    myprojectの作成

--- myproject/myproject/local_settings.py
+++ myproject/myproject/local_settings.py
@@ -1,14 +1,3 @@
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 SECRET_KEY = 'ae3z()-rk&8n@kb8_g5$6j0(iv1h6mtfdk!*#tv6dkbf5dwvi5'
 
-DEBUG = True
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
