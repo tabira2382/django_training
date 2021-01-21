5dbbd6ef3720277711c1b5b52ee5cc2f97912959
tabira2382 <tabira2382@gmail.com>
2021-01-21 10:21:08 +0900

    MySQL設定完了

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sample',
+        'USER': 'root',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
