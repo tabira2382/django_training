398f038bb8e8ab98ebd93894a3b0dfd3cb6e05dc
tabira2382 <tabira2382@gmail.com>
2021-01-22 14:52:05 +0900

    SQLコマンド２件だけ入力

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,5 +1,15 @@
 from django.views.generic import TemplateView
 
+from .models import Post
+
 
 class Index(TemplateView):
     template_name = 'myapp/index.html'
+
+    def get_title_data(self, *args, **kwargs):
+        title = super().get_context_data(**kwargs)
+        post_list = Post.objects.all().order_by('-created_at')[:2]
+        title = {
+            'post_list': post_list,
+        }
+        return title
