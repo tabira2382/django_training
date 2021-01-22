f167cfcfd5590688b3843f65a90fbeee6cd69d88
tabira2382 <tabira2382@gmail.com>
2021-01-22 14:58:46 +0900

    SQLコマンド２件だけ入力変更

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -6,10 +6,10 @@ from .models import Post
 class Index(TemplateView):
     template_name = 'myapp/index.html'
 
-    def get_title_data(self, *args, **kwargs):
-        title = super().get_context_data(**kwargs)
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
         post_list = Post.objects.all().order_by('-created_at')[:2]
-        title = {
+        context = {
             'post_list': post_list,
         }
-        return title
+        return context
