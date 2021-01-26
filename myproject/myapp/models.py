f504e4c055185cc8ed1bd4149d41a1e196e6b673
tabira2382 <tabira2382@gmail.com>
2021-01-26 15:07:16 +0900

    Post_commentにForeingKeyを設定した

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -3,11 +3,14 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField('タイトル', max_length=50)
-    comment = models.TextField('コメント', max_length=1000)
-
-    def comment_number(self):
-        n = Post.objects.filter(comment=self).count()
-        return n
 
     def __str__(self):
         return self.title
+
+
+class Post_comment(models.Model):
+    comment = models.TextField()
+    post = models.ForeignKey(Post, verbose_name="コメント", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.comment
