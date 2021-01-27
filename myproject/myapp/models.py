96a3551651ea29349a20a7835e6e5f37ea93bd03
tabira2382 <tabira2382@gmail.com>
2021-01-28 01:06:54 +0900

    コメント数表示できた（動画視聴）

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -12,5 +12,9 @@ class Post_comment(models.Model):
     comment = models.TextField()
     post = models.ForeignKey(Post, verbose_name="コメント", on_delete=models.CASCADE)
 
+    def post_count(self):
+        n = Post_comment.objects.filter(comment=self).count()
+        return n
+
     def __str__(self):
         return self.comment
